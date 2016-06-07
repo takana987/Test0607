9efd238e029f915b13ea8604b8a6193df920a9ea
kunren16 <kunren16@macbook06.local>
2016-06-07 10:09:07 +0900

    1stコミット

---
+++ Test0607/ViewController02.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController02.swift
+//  Test0607
+//
+//  Created by kunren16 on 2016/06/07.
+//  Copyright © 2016年 IkedaRyutaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController02: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
